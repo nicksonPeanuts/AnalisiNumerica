c8bf48e4d140e900a01d82ea7d1abc51200640cb
Nicola Pinat <34040525+NickPne7@users.noreply.github.com>
2025-05-14 18:41:28 +0200

    fatto lgauss

--- lugauss.m
+++ lugauss.m
@@ -4,15 +4,16 @@
 
 function[L,U] = lugauss(A)
     n = size(A,1);
-    L = diag(n);
+    U = A;
+    L = eye(n);
     print(L)
     %colonna
-    for k = [1, n-1]
+    for k = 1: n-1
         %riga
-        for i = [k+1: n]
+        for i = k+1: n
             %coefficiente calcolato ogni volta, valori della matrice L
             L(i,k) = A(i,k)/A(k,k);
-            
+            U(i,:) = A(i,:) - L(i,k)*A(k,:); 
         end
     end
 end
